6e9e1f5c60bb484523b45dfef392ecc0c2a30b85
Queena <queena.qmx@gmail.com>
2023-08-03 16:30:55 -0500

    add naming and vars

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "env_code" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "private_cidr" {
+  type = list(any)
+}
+
+variable "public_cidr" {
+  type = list(any)
+}
+

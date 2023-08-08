6a8da2f9067b44c7716be7ef83f57daec6f7537e
Queena <queena.qmx@gmail.com>
2023-08-08 09:17:50 -0500

    add https ingress

--- instance.tf
+++ instance.tf
@@ -46,6 +46,15 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["108.255.250.119/32"]
   }
 
+  ingress {
+    description = "HTTPS ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0

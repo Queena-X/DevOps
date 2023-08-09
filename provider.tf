7b0eeabfd09f3616348743aedc656d1074918b66
Queena <queena.qmx@gmail.com>
2023-08-09 13:18:02 -0500

    add remote backup

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-devops-learn"
+    key    = "myapp/dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-devops"
+  }
 }
 
 provider "aws" {

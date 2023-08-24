a97aaa50361b9a681d1a59e36a48ded79aff95f0
Queena <queena.qmx@gmail.com>
2023-08-24 15:36:33 -0500

    add acm

--- modules/elb/route-53.tf
+++ modules/elb/route-53.tf
@@ -1,3 +1,16 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "test"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 data "aws_route53_zone" "hosted_zone" {
   name = var.domain_name
 }
@@ -7,5 +20,12 @@ resource "aws_route53_record" "devops_example" {
   name    = var.record_name
   type    = "CNAME"
   ttl     = 300
-  records = [aws_lb.public-alb]
+  records = [aws_lb.public-alb.dns_name]
 }
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.devops_example : record.fqdn]
+}
+
+
